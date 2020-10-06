0f31207bcdbf1abdd58c71a80158fcc09527874d
Anna Green <a.green@windowslive.com>
2020-10-07 08:53:53 +1300

    minesweeper

--- minesweeper.js
+++ minesweeper.js
@@ -1,18 +1,48 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+// I want to create a square
+  var board = {
+    cells: [
+      {row: 0, col: 0, isMine: true, hidden: true, isMarked: true},
+      {row: 0, col: 1, isMine: false, hidden: true, isMarked: true},
+      {row: 0, col: 2, isMine: false, hidden: true, isMarked: true
+      },
+
+      {row: 1, col: 0, isMine: false, hidden: true, isMarked: 0},
+      {row: 1, col: 1, isMine: true, hidden: true, isMarked: 0},
+      {row: 1, col: 2, isMine: false, hidden: true, isMarked: 0
+      },
+      
+      {row: 2, col: 0, isMine: false, hidden: true, isMarked: 0},
+      {row: 2, col: 1, isMine: false, hidden: true, isMarked: 0},
+      {row: 2, col: 2, isMine: true, hidden: true, isMarked: 0
+      }
+
+
+    ]
+
+  }
 
 function startGame () {
+
+  for(var i = 0; i < board.cells.length; i++)
+
+  board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
+
 
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
+  for( var = i; i < board.cells.length; i++) {
+
+    if board.cells[i] == true
+  }
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
